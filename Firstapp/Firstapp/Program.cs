2402a66d8dd29f90698f334e508c52113c624117
fahim25 <55046623+fahim25@users.noreply.github.com>
2019-09-09 21:40:26 +0600

    Project_add

---
+++ Firstapp/Firstapp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, address;
+
+            Console.WriteLine("Please Enter Your Name:");
+                name = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Your Age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Your Address:");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Your Contract:");
+            int contact = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Name " + name + "");
+            Console.WriteLine("Age " + age + "");
+            Console.WriteLine("Address " + address + "");
+            Console.WriteLine("Contact " + contact + "");
+
+            Console.ReadKey();
+        } 
+    }
+}
